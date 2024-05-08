973622552162f16aaf3274fd43840458909926a8
azuriye <derwaffenbarnitzke@gmail.com>
2024-05-08 18:25:42 +0400

    Now supports multiple URLs in a single message

--- bot.py
+++ bot.py
@@ -44,42 +44,45 @@ async def on_message(message):
             content = message.content
 
             if 'twitter.com' in content or 'x.com' in content:
-                url = ""
-                try:
-                    url = re.search(r'(https?://(?:twitter\.com/[^\s]*?/status/(\d{19})|x\.com/[^\s]*?/status/(\d{19})))', content).group(1)
-                except AttributeError:
-                    return False
+                urls = re.findall(r'(https?://(?:twitter\.com/[^\s]*?/status/(\d{19})|x\.com/[^\s]*?/status/(\d{19})))', content)
+
+                if not urls:
+                    return
 
                 if message.attachments:
                     return
 
-                attachments = []
-                j = CombinedJob(url)
-                j.run()
-                async with aiohttp.ClientSession() as session:
-                    for content_url, kwdict in zip(j.urls, j.kwdicts):
-                        async with session.get(content_url) as resp:
-                            tweet_date = kwdict['date'].strftime('%d.%m.%Y')
-                            tweet_id = str(kwdict['tweet_id'])
-                            extension = "."+kwdict['extension']
-                            image_num = "_"+str(+kwdict['num'])
-                            filename = tweet_date+"."+tweet_id+image_num+extension
-                            attachment = File(BytesIO(await resp.read()), filename=filename)
-                        attachments.append(attachment)
+                for url, *_ in urls:
+                    attachments = []
+                    j = CombinedJob(url)
+                    j.run()
+
+                    async with aiohttp.ClientSession() as session:
+                        for content_url, kwdict in zip(j.urls, j.kwdicts):
+                            async with session.get(content_url) as resp:
+                                tweet_date = kwdict['date'].strftime('%d.%m.%Y')
+                                tweet_id = str(kwdict['tweet_id'])
+                                extension = "."+kwdict['extension']
+                                image_num = "_"+str(+kwdict['num'])
+                                filename = tweet_date+"."+tweet_id+image_num+extension
+                                attachment = File(BytesIO(await resp.read()), filename=filename)
+                            attachments.append(attachment)
                     
-                    if attachments:
-                        tweet_author = kwdict['author']['name']
-                        tweet_nick = kwdict['author']['nick']
-                        tweet_content = kwdict['content']
+                        if attachments:
+                            tweet_author = kwdict['author']['name']
+                            tweet_nick = kwdict['author']['nick']
+                            tweet_content = kwdict['content']
+                            tweet_link = f'https://twitter.com/{tweet_author}/status/{tweet_id}'
 
-                        tweet_replies = human_format(kwdict['reply_count'])
-                        tweet_retweets = human_format(kwdict['retweet_count'])
-                        tweet_likes = human_format(kwdict['favorite_count'])
+                            tweet_replies = human_format(kwdict['reply_count'])
+                            tweet_retweets = human_format(kwdict['retweet_count'])
+                            tweet_likes = human_format(kwdict['favorite_count'])
 
-                        embed = Embed(title=f'{tweet_nick} (@{tweet_author})', description=f'{tweet_content}\n\n💬 {tweet_replies}\t🔁 {tweet_retweets}\t💖 {tweet_likes}', url=f'https://twitter.com/{tweet_author}/status/{tweet_id}', colour=Colour.blue())
-                        embed.set_footer(text="Date: "+ '/'.join(tweet_date.split('.')))
-                        await message.channel.send(files=attachments, embed=embed)
-                        await message.delete()
+                            embed = Embed(title=f'{tweet_nick} (@{tweet_author})',  description=f'{tweet_content}', url=tweet_link, colour=Colour.blue())
+                            embed.set_author(name=f'💬 {tweet_replies}   🔁 {tweet_retweets}   💖 {tweet_likes}', url=tweet_link)
+                            embed.set_footer(text="Date: "+ '/'.join(tweet_date.split('.')))
+                            await message.channel.send(files=attachments, embed=embed)
+                await message.delete()
                                                   
     except Exception as e:
         print("Error: ", e)
